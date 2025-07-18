dd87223352608f0f31a2810cde3fafe6313cc79a
Techloop Tamil <contact.techlooptamil@gmail.com>
2025-07-18 14:47:48 +0530

    Update Techloop_Tamil038.sh

--- Entity and Sentiment Analysis with the Natural Language API /Techloop_Tamil038.sh
+++ Entity and Sentiment Analysis with the Natural Language API /Techloop_Tamil038.sh
@@ -6,7 +6,7 @@ gcloud services enable apikeys.googleapis.com
 
 export ZONE=$(gcloud compute instances list --filter="name=('linux-instance')" --format="value(zone)")
 
-gcloud alpha services api-keys create --display-name="techcps"
+gcloud alpha services api-keys create --display-name="Techloot_Tamil"
 
 KEY_NAME=$(gcloud alpha services api-keys list --format="value(name)" --filter="displayName=techcps")
 API_KEY=$(gcloud alpha services api-keys get-key-string $KEY_NAME --format="value(keyString)")
